fb8ef977237f96cc210bd3c1502aa374490f7b5a
AlonGenosar <alon@bootleg.co.il>
2019-04-06 01:12:55 +0300

    a

--- Logger.swift
+++ Logger.swift
@@ -3,23 +3,23 @@
 //  Snappers
 //
 //  Created by Alon Genosar on 07/02/2019.
-//  Copyright © 2019 Snappers. All rights reserved.
+//  Copyright © 2019 Alon Genosar. All rights reserved.
 //
 
 import UIKit
-enum LoggerLevelThreshold:Int { case info,warning,error,off }
-enum LoggerLevel:Int { case info,warning,error }
+public enum LoggerLevelThreshold:Int { case info,warning,error,off }
+public enum LoggerLevel:Int { case info,warning,error }
 enum LoggerEmojis:String { case info = "🔵", warning = "🔶", error = "🔴", heart = "♥️", beark = "🐻", unicorn = "🦄", skull = "☠️", pumpkin = "🎃", pig = "🐷", mushroom = "🍄", star = "⭐️",leaf  = "☘️" }
-class Logger {
+open class Logger {
     private static let defaultLogLevel:LoggerLevel = .info
     private static var levelPrefix = [LoggerEmojis.info.rawValue + "info:",LoggerEmojis.warning.rawValue+"warning:",LoggerEmojis.error.rawValue,"error:"]
     static var level:LoggerLevelThreshold = .info
     static var showLevelPrefix:Bool = true
     class func log(level:LoggerLevel = defaultLogLevel,_ items:Any ...,prefixOverride:String? = nil) { _log(level: level, items, prefixOverride: prefixOverride) }
-    class func set(prefix:String,forLevel level:LoggerLevel) { levelPrefix[level.rawValue] = prefix}
-    class func info(    _ items:Any ..., prefixOverride:String? = nil) { _log(level: .info, items,prefixOverride: prefixOverride) }
-    class func warning( _ items:Any ..., prefixOverride:String? = nil) { _log(level: .warning, items,prefixOverride: prefixOverride) }
-    class func error(   _ items:Any ..., prefixOverride:String? = nil) { _log(level: .error, items,prefixOverride: prefixOverride) }
+    public class func set(prefix:String,forLevel level:LoggerLevel) { levelPrefix[level.rawValue] = prefix}
+    public class func info(    _ items:Any ..., prefixOverride:String? = nil) { _log(level: .info, items,prefixOverride: prefixOverride) }
+    public class func warning( _ items:Any ..., prefixOverride:String? = nil) { _log(level: .warning, items,prefixOverride: prefixOverride) }
+    public class func error(   _ items:Any ..., prefixOverride:String? = nil) { _log(level: .error, items,prefixOverride: prefixOverride) }
     class private func _log(level:LoggerLevel = defaultLogLevel,_ items:[Any],prefixOverride:String? = nil) {
         if level.rawValue >= self.level.rawValue {
             var m = [String]()
